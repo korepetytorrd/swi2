ec2fe6ec5e6149abfe08f9e4d29c003300e17584
Radek <radoslaw.danielkiewicz@pollub.edu.pl>
2023-12-27 09:33:43 +0100

    modyfikacja main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Christmastree christmastree = new Christmastree();
-        //christmastree.Budowa(10);
+        christmastree.Budowa(10);
         christmastree.BudowaR(10,0);
 
 
